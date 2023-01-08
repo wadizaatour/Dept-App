1063d0aba64bb25a686c1030dca6214bd40c0d6d
wadizaatour <zaatourwadi1@gmail.com>
2023-01-08 16:01:58 +0100

    feat: add font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
+        maison:['Maison Neue',...defaultTheme.fontFamily.sans]
       },
     },
   },
