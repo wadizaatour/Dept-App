cf9290096c84de31608c3188bde333be53a96d02
wadizaatour <zaatourwadi1@gmail.com>
2023-01-08 19:05:59 +0100

    add validation form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,22 +7,72 @@
                 TO HELP!
             </p>
         </div>
-        <div id="form" class="">
+        <Form id="form" class="" @submit="onSubmit">
             <div id="contact" class="flex flex-row gap-9">
-                <Inputs labelTitle="Name" typeInput="text" />
-                <Inputs labelTitle="Email" typeInput="email" />
+                <Input labelTitle="Name" name="name" typeInput="text" :validationRules="validateName"/>
+                <Input labelTitle="Email" name="email" typeInput="email" :validationRules="validateEmail" />
             </div>
             <div id="message" class="flex pt-10">
-                <Inputs labelTitle="Message" typeInput="text" />
+                <Input labelTitle="Message" name="message" typeInput="text" :validationRules="validateMessage" />
             </div>
             <div class="pt-20">
-            <input type="submit" value="SEND" class="btn rounded-3xl px-10 text-base border-black border"/>  
+                <input type="submit" value="SEND" class="btn rounded-3xl px-10 text-base border-black border" />
             </div>
-        </div>
+        </Form>
     </div>
 </template>
  
 <script lang="ts" setup>
-import Inputs from "./Inputs.vue"
+import Input from "./Input.vue"
+import { Form, RuleExpression } from 'vee-validate';
+
+const isRequired = (value:string): string |undefined => {
+    if (!value) {
+        return 'This field is required';
+    }
+}
+
+const onSubmit = (values: any) => {
+    console.log(values)
+}
+
+const validateEmail = (value:string) => {
+    if (!value) {
+        return 'This field is required';
+    }
+
+    // if the field is not a valid email
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    if (!regex.test(value)) {
+        return 'This field must be a valid email';
+    }
+    // All is good
+    return true;
+};
+
+const validateName = (value:string) =>{
+    if (!value) {
+        return 'This field is required';
+    }
+
+    if (value.length < 5) {
+        return 'This field must have a length of 5 characters'
+    }
+
+    return true;
+}
+
+const validateMessage = (value:string) =>{
+    if (!value) {
+        return 'This field is required';
+    }
+
+    if (value.length < 20) {
+        return 'This field must have a length of 20 characters'
+    }
+
+    return true;
+}
+
 </script>
 
---
+++ src/components/Input.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col ">
+        <label class="text-base">{{ labelTitle }}</label>
+        <Field 
+            :type="typeInput" 
+            :name="name"
+            :rules="(validationRules as any)"
+            required
+            class="border-b-black border-b hover:none" 
+        />
+        <ErrorMessage :name="name" class="text-red-600"/>
+    </div>
+</template>
+<script lang="ts" setup>
+import { ErrorMessage, Field, GenericValidateFunction, RuleExpression } from 'vee-validate';
+
+defineProps<{ 
+    labelTitle: string,
+    typeInput: string,
+    name: string,
+    validationRules: (value:string) => any
+}>()
+</script>
