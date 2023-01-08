eae82ed2dafb0a93bb782604f0ce8027082010b5
wadizaatour <zaatourwadi1@gmail.com>
2023-01-08 13:11:21 +0100

    adding dynamic menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,34 @@
-<template> 
-<nav class="flex items-center justify-between h-[115px] bg-[#121212] px-14">
-      <a href="https://dept.com/" class="flex items-center">
-          <img src="../assets/deptLogo.svg"  alt="Flowbite Logo" />
-      </a>
-      <div class="flex items-center justify-between gap-24">
-        <ul class="flex flex-row justify-center items-center gap-12" >
-          <li v-for="item in MenuItem " class="text-lg font-normal leading-4 hover:underline text-white">
-            <a href="#" aria-current="page" :key="item">{{ item }}</a>
-          </li>
-        </ul>
-        <button>  <img src="../assets/dots.svg"  alt="Menu" /></button>
+<template>
+  <nav class="flex items-center justify-between bg-[#121212] px-14" :class="showMenu">
+    <a href="https://dept.com/" class="flex items-center">
+      <img src="../assets/deptLogo.svg" alt="Flowbite Logo" />
+    </a>
+    <div class="flex items-center justify-between gap-24">
+      <ul class="flex flex-row justify-center items-center gap-12">
+        <li v-for="item in Menu " class="text-lg font-normal leading-4 hover:underline text-white">
+          <a href="#" aria-current="page" :key="item">{{ item }}</a>
+        </li>
+      </ul>
+      <button @click="toggleMenu"> <img src="../assets/dots.svg" alt="Menu" /></button>
     </div>
   </nav>
 </template>
 <script setup lang="ts">
-    const MenuItem =  ["WORK","CULTURE","SERVICES","INSIGHTS","CARRERS", "CONTACT"]
+import { computed, ref } from "vue"
+import { MenuTitles } from "../models/menu"
+
+let isOpen = ref(false)
+const Menu = Object.values(MenuTitles)
+
+const toggleMenu = () => {
+  isOpen.value = !isOpen.value;
+}
+const showMenu = computed(() => {
+  console.log(isOpen.value)
+    if(isOpen.value) {
+      return "h-screen"
+    } else {
+      return "h-[115px]"
+    }
+});
 </script>
---
+++ src/models/menu.ts
@@ -0,0 +1,8 @@
+export enum MenuTitles {
+    Work = "WORK",
+    Culture = "CULTURE",
+    Services = "SERVICES",
+    Insights = "INSIGHTS",
+    Carrers = "CARRERS",
+    contact = "CONTACT"
+}
