d894edfbfc92c97bebbb54cf5346dc620e3ecf93
wadizaatour <zaatourwadi1@gmail.com>
2023-01-11 10:12:26 +0100

    adding dynamic route

--- src/components/FilterList.vue
+++ src/components/FilterList.vue
@@ -6,16 +6,20 @@
         <span class="p-8 text-[#A3A3A3] max-w-xs">In
             <!-- <button class="text-black">All Industry</button> -->
             <label for="Industry" class="text-black">All Industry</label>
-            <select v-model="IndustryValue" name="Industry" id="Industry">
+            <select v-model="IndustryValue" name="Industry" id="Industry" @change="updateRoute">
                 <option v-for="name in IndustryName" :value="name">{{ name }}</option>               
         </select>
         </span>
     </div>
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
 import {Industry} from "../models/client"
-
+import { useRouter } from 'vue-router'
+import { ref, watch } from 'vue'
 const IndustryValue = ref('')
+const router = useRouter()
 const IndustryName = Object.values(Industry)
+const updateRoute = () => {
+    router.push({ name: 'Industry', params: { industry: IndustryValue.value } })
+}
 </script>
--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,11 @@ const routes = [
     name: "Home",
     component: Home,
   },
+  {
+    path: "/:industry",
+    name: "Industry",
+    component: Home,
+  },
  
 ];
 
